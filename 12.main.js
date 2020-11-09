03ff74b81f9ea9a17d097744c8b72761eb312dba
Jim Geersinga <j.geersinga@outlook.com>
2020-11-09 14:31:59 +0100

    add more assignments

---
+++ 12.main.js
@@ -0,0 +1,5 @@
+var hond = new Hond("Joep");
+var kat = new Kat("Loesje");
+
+console.log("Hond:", hond, hond.getNaam(), hond.getAantalPoten(), hond.spreek());
+console.log("Kat:", kat, kat.getNaam(), kat.getAantalPoten(), kat.spreek());
